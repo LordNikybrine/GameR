c5d5b4b7605a6be4c4cfd31d949ddbb3cc84b0c3
Nick <nikybrine.jung@gmail.com>
2023-10-28 14:54:42 +0200

    changes for sound and tutorial level

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,13 @@ extends Node2D
 var menu = null
 var instance = null
 
+func _physics_process(delta):
+	$Music_Player.volume_db = Global.music_vol / 2 - 40
+	if Global.music_vol == 0:
+		$Music_Player.stream_paused = true
+	elif Global.music_vol > 0:
+		$Music_Player.stream_paused = false
+
 func _ready():
 	add_level()
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 var run_speed = 350
 var jump_speed = -900
+var gravity = 2000
+var flip = null
 
 func _physics_process(delta):
+	velocity.y += gravity * delta
 	get_input()
 	move_and_slide()
+	animations()
+	check_flip()
 
 func get_input():
 	velocity.x = 0
@@ -19,3 +24,28 @@ func get_input():
 		velocity.x += run_speed
 	if left:
 		velocity.x -= run_speed
+
+func animations():
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_pressed("up")
+	
+	if left || right:
+		$AnimatedSprite2D.play("run")
+	if left:
+		flip = true
+	elif right: 
+		flip = false
+	
+	if velocity.x == 0 && velocity.y == 0:
+		$AnimatedSprite2D.play("idle")
+	
+	if jump:
+		$AnimatedSprite2D.play("jump")
+
+
+func check_flip():
+	if flip:
+		$AnimatedSprite2D.scale.x = -1
+	elif !flip:
+		$AnimatedSprite2D.scale.x = 1
